9511dc420bf25f193e44e3e417b45b45ede82634
jay <jay.zhangjunjie@outlook.com>
2023-08-03 23:40:28 +0800

    ✨ feat(main.py):
    updata

--- main.py
+++ main.py
@@ -8,8 +8,16 @@ app = MBPollTCPApp(ipAddress="172.16.11.31")
 # app.closeConnection()
 app.openConnection()
 
-
-
+doc = app.createReadCoilsDoc(1, 528, 16)
+doc.showWindow()
 import time
 while 1:
     time.sleep(1)
+    # values = [doc.getAddress(528+i).value for i in range(16)]
+    # values = doc.
+    values = doc.readCoils(528, 16)
+    print(values)
+    print(doc.getRxCount())
+    print(doc.getTxCount())
+    doc.showWindow()
+    app.showCommunicationTraffic()
