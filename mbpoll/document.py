dddc3869c7327d384271fd7951100cd78011bc2c
jay <jay.zhangjunjie@outlook.com>
2023-08-06 23:07:28 +0800

    🦄 refactor(document): 增加对不同数据格式的支持

--- mbpoll/document.py
+++ mbpoll/document.py
@@ -1,7 +1,8 @@
 
+import time
 from typing import Union
 import win32com.client as win32
-from mbpoll.defines import IncorrectAddressError, DataFormat, FunctionCode, ReadWriteResult
+from mbpoll.defines import ByteOrder, IncorrectAddressError, DataFormat, FunctionCode, ReadWriteResult
 
  
 class MBPollBaseDocument():
@@ -195,6 +196,116 @@ class MBPollDocument(MBPollBaseDocument):
         super().__init__(slaveID, scanRate)
         self.functionCode = None
         # self.__readAddress = None
+        
+    
+    def __getValue(self, address:int, quantity:int, format:DataFormat) -> list:
+        if format in [DataFormat.FLOAT_BIG_ENDIAN, DataFormat.DOUBLE_BIG_ENDIAN, DataFormat.SIGNED_32B_BIG_ENDIAN, DataFormat.UNSIGNED_32B_BIG_ENDIAN, DataFormat.SIGNED_64B_BIG_ENDIAN, DataFormat.UNSIGNED_64B_BIG_ENDIAN]:
+            self.document.ByteOrder = ByteOrder.BIG_ENDIAN
+        elif format in [DataFormat.FLOAT_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.DOUBLE_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_32B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_32B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_64B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_64B_LITTLE_ENDIAN_BYTE_SWAP]:
+            self.document.ByteOrder = ByteOrder.LITTLE_ENDIAN_BYTE_SWAP
+        elif format in [DataFormat.FLOAT_BIG_ENDIAN_BYTE_SWAP, DataFormat.DOUBLE_BIG_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_32B_BIG_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_32B_BIG_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_64B_BIG_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_64B_BIG_ENDIAN_BYTE_SWAP]:
+            self.document.ByteOrder = ByteOrder.BIG_ENDIAN_BYTE_SWAP
+        elif format in [DataFormat.FLOAT_LITTLE_ENDIAN, DataFormat.DOUBLE_LITTLE_ENDIAN, DataFormat.SIGNED_32B_LITTLE_ENDIAN, DataFormat.UNSIGNED_32B_LITTLE_ENDIAN, DataFormat.SIGNED_64B_LITTLE_ENDIAN, DataFormat.UNSIGNED_64B_LITTLE_ENDIAN]:
+            self.document.ByteOrder = ByteOrder.LITTLE_ENDIAN
+        if format in [DataFormat.SIGNED, DataFormat.HEX, DataFormat.BINARY]:
+            values = [self.document.SRegisters(index) for index in range(quantity)]
+        elif format == DataFormat.UNSIGNED:
+            values = [self.document.URegisters(index) for index in range(quantity)]
+        elif format in [DataFormat.FLOAT_BIG_ENDIAN, DataFormat.FLOAT_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.FLOAT_BIG_ENDIAN_BYTE_SWAP, DataFormat.FLOAT_LITTLE_ENDIAN]:
+            values = [self.document.Floats(index) for index in range(0, quantity, 2)]
+        elif format in [DataFormat.DOUBLE_BIG_ENDIAN, DataFormat.DOUBLE_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.DOUBLE_BIG_ENDIAN_BYTE_SWAP, DataFormat.DOUBLE_LITTLE_ENDIAN]:
+            values = [self.document.Doubles(index) for index in range(0, quantity, 4)]
+        elif format in [DataFormat.SIGNED_32B_BIG_ENDIAN, DataFormat.SIGNED_32B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_32B_BIG_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_32B_LITTLE_ENDIAN]:
+            values = [self.document.Ints_32(index) for index in range(0, quantity, 2)]
+        elif format in [DataFormat.UNSIGNED_32B_BIG_ENDIAN, DataFormat.UNSIGNED_32B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_32B_BIG_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_32B_LITTLE_ENDIAN]:
+            values = [self.document.UInts_32(index) for index in range(0, quantity, 2)]
+        elif format in [DataFormat.SIGNED_64B_BIG_ENDIAN, DataFormat.SIGNED_64B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_64B_BIG_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_64B_LITTLE_ENDIAN]:
+            values = [self.document.Ints_64(index) for index in range(0, quantity, 4)]
+        elif format in [DataFormat.UNSIGNED_64B_BIG_ENDIAN, DataFormat.UNSIGNED_64B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_64B_BIG_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_64B_LITTLE_ENDIAN]:
+            values = [self.document.UInts_64(index) for index in range(0, quantity, 4)]
+        return values
+    
+    
+    def __setValue(self, address:int, values:list, format:DataFormat, isWinVersion:bool=False) -> None:
+        #! ByteOrder 不控制Win地址
+        if not isWinVersion:
+            if format in [DataFormat.FLOAT_BIG_ENDIAN, DataFormat.DOUBLE_BIG_ENDIAN, DataFormat.SIGNED_32B_BIG_ENDIAN, DataFormat.UNSIGNED_32B_BIG_ENDIAN, DataFormat.SIGNED_64B_BIG_ENDIAN, DataFormat.UNSIGNED_64B_BIG_ENDIAN]:
+                self.document.ByteOrder = ByteOrder.BIG_ENDIAN
+            elif format in [DataFormat.FLOAT_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.DOUBLE_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_32B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_32B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_64B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_64B_LITTLE_ENDIAN_BYTE_SWAP]:
+                self.document.ByteOrder = ByteOrder.LITTLE_ENDIAN_BYTE_SWAP
+            elif format in [DataFormat.FLOAT_BIG_ENDIAN_BYTE_SWAP, DataFormat.DOUBLE_BIG_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_32B_BIG_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_32B_BIG_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_64B_BIG_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_64B_BIG_ENDIAN_BYTE_SWAP]:
+                self.document.ByteOrder = ByteOrder.BIG_ENDIAN_BYTE_SWAP
+            elif format in [DataFormat.FLOAT_LITTLE_ENDIAN, DataFormat.DOUBLE_LITTLE_ENDIAN, DataFormat.SIGNED_32B_LITTLE_ENDIAN, DataFormat.UNSIGNED_32B_LITTLE_ENDIAN, DataFormat.SIGNED_64B_LITTLE_ENDIAN, DataFormat.UNSIGNED_64B_LITTLE_ENDIAN]:
+                self.document.ByteOrder = ByteOrder.LITTLE_ENDIAN
+
+        startIndex = address - self.startingAddress
+        startIndex = 0
+        if format == DataFormat.SIGNED:
+            if isWinVersion:
+                [self.document.SRegistersWin(index+startIndex, values[index]) for index in range(len(values))]
+            else:
+                [self.document.SRegisters(index+startIndex, values[index]) for index in range(len(values))]
+                return len(values)
+        elif format in [DataFormat.UNSIGNED, DataFormat.HEX, DataFormat.BINARY]:
+            if isWinVersion:
+                [self.document.URegistersWin(index+startIndex, values[index]) for index in range(len(values))]
+            else:
+                [self.document.URegisters(index+startIndex, values[index]) for index in range(len(values))]
+                return len(values)
+        elif format in [DataFormat.FLOAT_BIG_ENDIAN, DataFormat.FLOAT_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.FLOAT_BIG_ENDIAN_BYTE_SWAP, DataFormat.FLOAT_LITTLE_ENDIAN]:
+            if isWinVersion:
+                [self.document.FloatsWin(index*2+startIndex, values[index]) for index in range(len(values))]
+            else:
+                [self.document.Floats(index*2+startIndex, values[index]) for index in range(len(values))]
+                return len(values) * 2
+        elif format in [DataFormat.DOUBLE_BIG_ENDIAN, DataFormat.DOUBLE_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.DOUBLE_BIG_ENDIAN_BYTE_SWAP, DataFormat.DOUBLE_LITTLE_ENDIAN]:
+            if isWinVersion:
+                [self.document.DoublesWin(index*4+startIndex, values[index]) for index in range(len(values))]
+            else:
+                [self.document.Doubles(index*4+startIndex, values[index]) for index in range(len(values))]
+                return len(values) * 4
+        elif format in [DataFormat.SIGNED_32B_BIG_ENDIAN, DataFormat.SIGNED_32B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_32B_BIG_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_32B_LITTLE_ENDIAN]:
+            if isWinVersion:
+                [self.document.Ints_32Win(index*2+startIndex, values[index]) for index in range(len(values))]
+            else:
+                [self.document.Ints_32(index*2+startIndex, values[index]) for index in range(len(values))]
+                return len(values) * 2
+        elif format in [DataFormat.UNSIGNED_32B_BIG_ENDIAN, DataFormat.UNSIGNED_32B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_32B_BIG_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_32B_LITTLE_ENDIAN]:
+            if isWinVersion:
+                [self.document.UInts_32Win(index*2+startIndex, values[index]) for index in range(len(values))]
+            else:
+                [self.document.UInts_32(index*2+startIndex, values[index]) for index in range(len(values))]
+                return len(values) * 2
+        elif format in [DataFormat.SIGNED_64B_BIG_ENDIAN, DataFormat.SIGNED_64B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_64B_BIG_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_64B_LITTLE_ENDIAN]:
+            if isWinVersion:
+                [self.document.Ints_64Win(index*4+startIndex, values[index]) for index in range(len(values))]
+            else:
+                [self.document.Ints_64(index*4+startIndex, values[index]) for index in range(len(values))]
+                return len(values) * 4
+        elif format in [DataFormat.UNSIGNED_64B_BIG_ENDIAN, DataFormat.UNSIGNED_64B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_64B_BIG_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_64B_LITTLE_ENDIAN]:
+            if isWinVersion:
+                [self.document.UInts_64Win(index*4+startIndex, values[index]) for index in range(len(values))]
+            else:
+                [self.document.UInts_64(index*4+startIndex, values[index]) for index in range(len(values))]
+                return len(values) * 4
+            
+    def __getNums(self, values:list, format:DataFormat):
+        if format == DataFormat.SIGNED:
+            return len(values)
+        elif format in [DataFormat.UNSIGNED, DataFormat.HEX, DataFormat.BINARY]:
+            return len(values)
+        elif format in [DataFormat.FLOAT_BIG_ENDIAN, DataFormat.FLOAT_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.FLOAT_BIG_ENDIAN_BYTE_SWAP, DataFormat.FLOAT_LITTLE_ENDIAN]:
+            return len(values) * 2
+        elif format in [DataFormat.DOUBLE_BIG_ENDIAN, DataFormat.DOUBLE_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.DOUBLE_BIG_ENDIAN_BYTE_SWAP, DataFormat.DOUBLE_LITTLE_ENDIAN]:
+            return len(values) * 4
+        elif format in [DataFormat.SIGNED_32B_BIG_ENDIAN, DataFormat.SIGNED_32B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_32B_BIG_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_32B_LITTLE_ENDIAN]:
+            return len(values) * 2
+        elif format in [DataFormat.UNSIGNED_32B_BIG_ENDIAN, DataFormat.UNSIGNED_32B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_32B_BIG_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_32B_LITTLE_ENDIAN]:
+            return len(values) * 2
+        elif format in [DataFormat.SIGNED_64B_BIG_ENDIAN, DataFormat.SIGNED_64B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_64B_BIG_ENDIAN_BYTE_SWAP, DataFormat.SIGNED_64B_LITTLE_ENDIAN]:
+            return len(values) * 4
+        elif format in [DataFormat.UNSIGNED_64B_BIG_ENDIAN, DataFormat.UNSIGNED_64B_LITTLE_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_64B_BIG_ENDIAN_BYTE_SWAP, DataFormat.UNSIGNED_64B_LITTLE_ENDIAN]:
+            return len(values) * 4
     
     
     def readCoils(self, address:int, quantity:int = 1) -> list:
@@ -212,6 +323,7 @@ class MBPollDocument(MBPollBaseDocument):
             self.functionCode = FunctionCode.READ_COILS
             self.startingAddress = address
             self.quantity = quantity
+            time.sleep(self.scanRate/1000)
         result = [self.document.Coils(index) for index in range(quantity)]
         return result
     
@@ -231,11 +343,12 @@ class MBPollDocument(MBPollBaseDocument):
             self.functionCode = FunctionCode.READ_DISCRETE_INPUTS
             self.startingAddress = address
             self.quantity = quantity
+            time.sleep(self.scanRate/1000)
         result = [self.document.Coils(index) for index in range(quantity)]
         return result
     
     
-    def readHoldingRegisters(self, address:int, quantity:int = 1):
+    def readHoldingRegisters(self, address:int, quantity:int = 1, format:DataFormat=DataFormat.SIGNED):
         """Modbus function code 03
 
         Args:
@@ -250,11 +363,12 @@ class MBPollDocument(MBPollBaseDocument):
             self.functionCode = FunctionCode.READ_HOLDING_REGISTERS
             self.startingAddress = address
             self.quantity = quantity
-        result = [self.document.SRegisters(index) for index in range(quantity)]
+            time.sleep(self.scanRate/1000)
+        result = self.__getValue(address, quantity, format)
         return result
     
     
-    def readInputRegisters(self, address:int, quantity:int = 1):
+    def readInputRegisters(self, address:int, quantity:int = 1, format:DataFormat=DataFormat.SIGNED):
         """Modbus function code 04
 
         Args:
@@ -269,7 +383,8 @@ class MBPollDocument(MBPollBaseDocument):
             self.functionCode = FunctionCode.READ_INPUT_REGISTERS
             self.startingAddress = address
             self.quantity = quantity
-        result = [self.document.SRegisters(index) for index in range(quantity)]
+            time.sleep(self.scanRate/1000)
+        result = self.__getValue(address, quantity, format)
         return result
     
     
@@ -277,56 +392,55 @@ class MBPollDocument(MBPollBaseDocument):
         if isCreateWin:
             scanRate = self.scanRate if scanRateForWin is None else scanRateForWin
             self.document.WriteSingleCoilWin(self.slaveID, address, scanRate)
-            self.document.CoilsWin(0, data)
             self.functionCode = FunctionCode.WRITE_SINGLE_COIL
             self.startingAddress = address
             self.quantity = 1
+            self.document.CoilsWin(0, int(bool(data)))
         else:
-            self.document.Coils(0, data)
+            self.document.Coils(0 + address - self.startingAddress, int(bool(data)))
             self.document.WriteSingleCoil(self.slaveID, address)
     
-    
-    def writeSingleRegister(self, address, data, isCreateWin:bool=False, scanRateForWin:int=None, enableRefresh:bool=True):
-        if isCreateWin:
-            scanRate = self.scanRate if scanRateForWin is None else scanRateForWin
-            self.document.WriteSingleRegisterWin(self.slaveID, address, scanRate)
-            self.document.SRegistersWin(0, data)
-            self.functionCode = FunctionCode.READ_INPUT_REGISTERS
-            self.startingAddress = address
-            self.quantity = 1
-        else:
-            self.document.SRegisters(0, data) 
-            self.document.WriteSingleRegister(self.slaveID, address)
+    # # todo:写单寄存器时 需要看当前是什么格式，并且只能写入一个寄存器
+    # def writeSingleRegister(self, address, data, format:DataFormat=DataFormat.SIGNED, isCreateWin:bool=False, scanRateForWin:int=None, enableRefresh:bool=True):
+    #     if isCreateWin:
+    #         scanRate = self.scanRate if scanRateForWin is None else scanRateForWin
+    #         self.document.WriteSingleRegisterWin(self.slaveID, address, scanRate)
+    #         self.functionCode = FunctionCode.READ_INPUT_REGISTERS
+    #         self.startingAddress = address
+    #         self.quantity = 1
+    #         self.__setValue(address, [data], format, isWinVersion=True)
+    #     else:
+    #         # todo
+    #         self.__setValue(address, [data], format, isWinVersion=False)
+    #         self.document.WriteSingleRegister(self.slaveID, address)
     
     
     def writeMultipleCoils(self, address, data:list, isCreateWin:bool=False, scanRateForWin:int=None, enableRefresh:bool=True):
         if isCreateWin:
             scanRate = self.scanRate if scanRateForWin is None else scanRateForWin
             self.document.WriteMultipleCoilsWin(self.slaveID, address, len(data), scanRate)
-            for index in range(len(data)):
-                self.document.CoilsWin(index, data[index])
             self.functionCode = FunctionCode.WRITE_MULTIPLE_COILS
             self.startingAddress = address
             self.quantity = 1
-        else:
-            self.document.WriteMultipleCoils(self.slaveID, address, len(data))
             for index in range(len(data)):
-                self.document.Coils(index, data[index])
+                self.document.CoilsWin(index, int(bool(data[index])))
+        else:
+            for index in range(len(data)):
+                self.document.Coils(index, int(bool(data[index])))
+            self.document.WriteMultipleCoils(self.slaveID, address, len(data))
     
     
-    def writeMultipleRegisters(self, address, data, isCreateWin:bool=False, scanRateForWin:int=None, enableRefresh:bool=True):
+    def writeMultipleRegisters(self, address, data, format:DataFormat=DataFormat.SIGNED,isCreateWin:bool=False, scanRateForWin:int=None, enableRefresh:bool=True):
         if isCreateWin:
             scanRate = self.scanRate if scanRateForWin is None else scanRateForWin
-            self.document.WriteMultipleRegistersWin(self.slaveID, address, len(data), scanRate)
-            for index in range(len(data)):
-                self.document.SRegistersWin(index, data[index])
+            self.document.WriteMultipleRegistersWin(self.slaveID, address, self.__getNums(data,format), scanRate)
             self.functionCode = FunctionCode.WRITE_MULTIPLE_REGISTERS
             self.startingAddress = address
-            self.quantity = 1
+            self.quantity = self.__getNums(data,format)
+            self.__setValue(address, data, format, isWinVersion=True)
         else:
-            self.document.WriteMultipleRegisters(self.slaveID, address, len(data))
-            for index in range(len(data)):
-                self.document.Registers(index, data[index])
+            self.__setValue(address, data, format, isWinVersion=False)
+            self.document.WriteMultipleRegisters(self.slaveID, address, self.__getNums(data,format))
 
     
     
